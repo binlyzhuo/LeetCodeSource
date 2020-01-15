7c3d1afa0b2767ca3af88ed69214b8c28437d388
binlyz <binlyzhuo@outlook.com>
2020-01-15 17:22:50 +0800

    ~

---
+++ LeetCodeSource/ArrayCode.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSource
+{
+    public class ArrayCode
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            
+            int p1 = m - 1;
+            int p2 = n - 1;
+            int p = m + n - 1;
+
+            while ((p1>=0)&&(p2>=0))
+            {
+                nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
+            }
+
+            //nums2.CopyTo(nums1,0);
+            foreach (var x in nums1)
+            {
+                Console.Write(x+",");
+            }
+        }
+
+        public int MajorityElement(int[] nums)
+        {
+            int num=0;
+            int max =0;
+
+            Dictionary<int,int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dict.ContainsKey(nums[i]))
+                {
+                    dict[nums[i]]++;
+
+                    if(dict[nums[i]]>max)
+                    {
+                        max = dict[nums[i]];
+                        num = nums[i];
+                    }
+                }
+                else
+                {
+                    dict.Add(nums[i],1);
+                }
+            }
+
+
+            //foreach (var kv in dict)
+            //{
+            //    if (kv.Value > max)
+            //    {
+            //        max = kv.Value;
+            //        num = kv.Key;
+            //    }
+            //}
+
+            return num;
+        }
+
+        public void Rotate(int[] nums, int k)
+        {
+
+        }
+    }
+}
--- LeetCodeSource/Program.cs
+++ LeetCodeSource/Program.cs
@@ -10,6 +10,77 @@ namespace LeetCodeSource
     {
         static void Main(string[] args)
         {
+            int count = TrailingZeroes(30);
+            int[] arr = { 2, 7, 11, 15 };
+            int day = MaxProfit(arr);
+
+            int[] numbers = { 2, 7, 11, 15 };
+            int[] indexs = TwoSum(numbers, 9);
+
+            ArrayCode arrayCode = new ArrayCode();
+            int[] a1 = {1, 2, 3, 0, 0, 0};
+            int[] a2 = { 2,5,6};
+            arrayCode.Merge(a1,3,a2,3);
+            Console.ReadLine();
+        }
+
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            int i = 0;
+            int j = numbers.Length - 1;
+
+            
+
+            while (i<j)
+            {
+                if (numbers[i] + numbers[j] == target)
+                {
+                    return new[] { ++i,++j};
+                }
+                else if(numbers[i]+numbers[j]>target)
+                {
+                    j--;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return new int[2];
+        }
+
+        public static int TrailingZeroes(int n)
+        {
+            int count = 0;
+            while (n > 0)
+            {
+                count += n / 5;
+                n = n / 5;
+            }
+            return count;
+
+        }
+
+        public static int MaxProfit(int[] prices)
+        {
+            int max = 0;
+            
+            for (int i = prices.Length - 1; i > 0; i--)
+            {
+                for (int j = i-1; j >=0; j--)
+                {
+                    int val = prices[i] - prices[j];
+                    if (max < val)
+                    {
+                        max = val;
+                        
+                    }
+                        
+                }
+            }
+
+            return max;
         }
     }
 }
