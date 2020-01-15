ef53ab7087b5d63bc527fc2477c4e9c971ec63dd
binlyzhuo <binlyzhuo@outlook.com>
2020-01-15 22:05:00 +0800

    00

--- LeetCodeSource/ArrayCode.cs
+++ LeetCodeSource/ArrayCode.cs
@@ -103,5 +103,22 @@ namespace LeetCodeSource
 
             return false;
         }
+
+        public int MissingNumber(int[] nums)
+        {
+            HashSet<int> hs = new HashSet<int>();
+            for(int i=0;i<nums.Length;i++)
+            {
+                hs.Add(nums[i]);
+            }
+
+            for(int i=0;i<=nums.Length;i++)
+            {
+                if (!hs.Contains(i))
+                    return i;
+            }
+
+            return 0;
+        }
     }
 }
